64ebc4ad10e1d8fe5cd19f00c6922e6a1bdf28ce
Jovan Samardžić <jsamardzic@gmail.com>
2022-04-04 16:36:47 +0200

    v2

--- kodovi/projekat.R
+++ kodovi/projekat.R
@@ -1,15 +1,8 @@
 install.packages("readxl")
-install.packages("writexl")
-install.packages("C50")
-install.packages("tree")
 install.packages("rpart")
 install.packages("rpart.plot")
 
-
 library("readxl")
-library("writexl")
-library("C50")
-library("tree")
 library("rpart")
 library("rpart.plot")
 
@@ -44,35 +37,31 @@ colnames(dfSimple) <- paste(replicate(19, "diff"), colnames(dfSimple), sep="")
 colnames(dfSimple)[1] <- "WL"
 
 
-write_xlsx(df, path = "./data/NBA_DataSet_Version2.xlsx")
-
-
 set.seed(1000)
 ###################################################################
 index <- sample(seq_len(nrow(df)), size = 0.75*nrow(df))
-training75 <- df[index,]
-pred75 <- df[-index,]
+trainingData <- df[index,]
+predictionData <- df[-index,]
 ###################################################################
 index <- sample(seq_len(nrow(dfSimple)), size = 0.75*nrow(dfSimple))
-training75 <- dfSimple[index,]
-pred75 <- dfSimple[-index,]
+trainingData <- dfSimple[index,]
+predictionData <- dfSimple[-index,]
 ###################################################################
-prop.table(table(training75$WL))
-prop.table(table(pred75$WL))
+prop.table(table(trainingData$WL))
+prop.table(table(predictionData$WL))
 
-model <- rpart(WL ~ .-diffPTS-diffFGpct-diffFGM-diffDREB, data = training75)
+model <- rpart(WL ~ .-diffPTS-diffFGpct-diffFGM-diffDREB, data = trainingData)
 rpart.plot(model)
 model$variable.importance
 model <- prune.rpart(model, cp=0.03) #plotcp(model) za odnos cp - sizeOfTree
 rpart.plot(model)
 
-model <- tree(WL ~ .-diffPTS, data = training75)
-model <- prune.tree(model, best=5) # broj listova
-plot(model)
-text(model, cex=0.5, pretty=0)
+results <- predict(model, predictionData, type = "class")
+sum(results == predictionData$WL)/nrow(predictionData)
 
-# model
-# summary(model)
 
-results <- predict(model, pred75, type = "class")
-sum(results == pred75$WL)/nrow(pred75)
+
+# model <- tree(WL ~ .-diffPTS, data = trainingData)
+# model <- prune.tree(model, best=5) # broj listova
+# plot(model)
+# text(model, cex=0.5, pretty=0)

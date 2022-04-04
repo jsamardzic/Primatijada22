1788ee2d192f5c9323bf653fdb5e9f485984d55e
Jovan Samardžić <jsamardzic@gmail.com>
2022-04-04 14:49:21 +0200

    test

--- kodovi/projekat.R
+++ kodovi/projekat.R
@@ -1,6 +1,6 @@
-#install.packages("readxl")
-#install.packages("writexl")
-#install.packages("C50")
+install.packages("readxl")
+install.packages("writexl")
+install.packages("C50")
 install.packages("tree")
 install.packages("rpart")
 install.packages("rpart.plot")
@@ -13,8 +13,8 @@ library("tree")
 library("rpart")
 library("rpart.plot")
 
-setwd("/Users/jovan.samke/Downloads/Primatijada")
-df <- read_xlsx("NBA_DataSet_Version1.xlsx")
+setwd("/Users/jovan.samke/Documents/GitHub/Primatijada22")
+df <- read_xlsx("./data/NBA_DataSet_Version1.xlsx")
 
 df <- na.omit(df)
 df <- df[-c(1:3, 5, 24)]
@@ -38,28 +38,26 @@ colnames(df)[c(5,11,23,29)] <- c("FGpct", "FTpct", "oppFGpct", "oppFTpct")
 rm(dfopp)
 rm(dfteam)
 
-write_xlsx(df, path = "NBA_DataSet_Version2.xlsx")
+write_xlsx(df, path = "./data/NBA_DataSet_Version2.xlsx")
 
-# Delimo df na deo za pravljenje i predikciju
-#
-#
 
-set.seed(1234)
+
+set.seed(1000)
 index <- sample(seq_len(nrow(df)), size = 0.75*nrow(df))
-t75 <- df[index,]
-p75 <- df[-index,]
+test75 <- df[index,]
+pred75 <- df[-index,]
 
-prop.table(table(t75$WL))
-prop.table(table(p75$WL))
+prop.table(table(test75$WL))
+prop.table(table(pred75$WL))
 
-# model <- C5.0(t75[,c(-1,-2,-3,-20,-21)],t75$WL)
+# model <- C5.0(test75[,c(-1,-2,-3,-20,-21)],test75$WL)
 
-model <- rpart(WL ~ .-PTS-oppPTS-FGpct-oppFGpct-FGM-oppFGM, data = t75)
+model <- rpart(WL ~ .-PTS-oppPTS-FGpct-oppFGpct-FGM-oppFGM, data = test75)
 rpart.plot(model)
 model$variable.importance
 #printcp(model)
 
-model <- tree(WL ~ .-PTS-oppPTS, data = t75)
+model <- tree(WL ~ .-PTS-oppPTS, data = test75)
 plot(model)
 text(model, cex=0.5)
 
@@ -67,5 +65,5 @@ text(model, cex=0.5)
 # model
 # summary(model)
 
-results <- predict(model, p75, type = "class")
-sum(results == p75$WL)/nrow(p75)
+results <- predict(model, pred75, type = "class")
+sum(results == pred75$WL)/nrow(pred75)
